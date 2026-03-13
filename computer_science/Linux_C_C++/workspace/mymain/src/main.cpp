d3396feeb8c209663a7687828b54bf99d120988a
hsf <1537259139@qq.com>
2026-03-13 22:38:37 +0800

    1st

--- computer_science/Linux_C_C++/workspace/mymain/src/main.cpp
+++ computer_science/Linux_C_C++/workspace/mymain/src/main.cpp
@@ -1,38 +1,6 @@
-#include <string>
-#include <thread>
-#include <unistd.h>
-
-
-class RDT_protocol {
-    struct transfer_data {
-        int type;
-        int seq; // start with 1
-        int len;
-        std::string data;
-    };
-
-    void Sender() { // lock
-        while (1) {
-            // send data
-            // receive ack
-        }
-
-    }
-
-    void Receiver() { // lock
-        while (1) {
-            // receive data
-            // send ack
-        }
-    }
-public:
-    void run() {
-        
-    }
-};
-
+#include <iostream>
 
 int main() {
-    pause();
-    _exit(0);
+    std::cout << __cplusplus << std::endl;
+    return 0;
 }
